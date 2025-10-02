b9ebe21c36bea87e7ad0ce89e654c000daf4c49d
yayam816 <yasya160806@gmail.com>
2025-10-02 07:40:10 +0700

    Modifikasi Perubahan 1

--- jobsheet5/ifCetakKRS26.java
+++ jobsheet5/ifCetakKRS26.java
@@ -10,6 +10,9 @@ public class ifCetakKRS26 {
     if (uktlunas) {
         System.out.println("Pembayaran UKT terverifikasi");
         System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+    } else {
+        System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
     }
+    
     } 
 }
