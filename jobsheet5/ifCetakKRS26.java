fd7373e4ae09dd81d5bb8a45384d132f9ebc00de
yayam816 <yasya160806@gmail.com>
2025-10-02 07:27:22 +0700

    Percobaan 1

---
+++ jobsheet5/ifCetakKRS26.java
@@ -0,0 +1,15 @@
+package Praktekdaspro.jobsheet5;
+
+import java.util.Scanner;
+public class ifCetakKRS26 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("--- Cetak KRS SIAKAD ---");
+    System.out.print("Apakah UKT sudah lunas? (true/false)");
+    boolean uktlunas = sc.nextBoolean();
+    if (uktlunas) {
+        System.out.println("Pembayaran UKT terverifikasi");
+        System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+    }
+    } 
+}
